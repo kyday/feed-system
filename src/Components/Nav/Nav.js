a5ed7ba79374d897cb5c61e5b9b42ea3085b2572
kiyeol <kyday63@gmail.com>
2021-03-22 20:27:00 +0900

    WIP

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,8 +3,8 @@ import "../../Styles/common.scss";
 
 function Nav(props) {
   return (
-    <nav>
-      <p>'[제출일] 이름'을 작성해주세요:)</p>
+    <nav className="navbar">
+      <p>'[3/23] 김기열'</p>
     </nav>
   );
 }
